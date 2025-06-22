aef4b0a782fb3a63897fbf08e9b1134bf5e9744c
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-06-22 15:47:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,19 @@ variable "location" {
   type    = string
   default = "eastus"
 }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "subnets" {
+  type = map(string)
+}
+
