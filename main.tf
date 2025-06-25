ee56d15b04bfaec0ab265194b371ef7389f70034
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-06-25 20:24:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "network" {
   source              = "./modules/network"
   vnet_name           = "infra-development"
-  resource_group_name = "Azure-infra-anand"
-  location            = "West India"
+  resource_group_name = var.resource_group_name
+  location            = var.location
   address_space       = ["10.0.0.0/16"]
 
   subnets = {
