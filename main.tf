83b0b3116b697e9dc1b3920b1035b0e598466c52
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-06-30 20:10:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,17 +10,7 @@ module "network" {
     aks-lb-subnet          = var.aks-lb-subnet
   }
 }
-#module "instances" {
-#  for_each = { for vm in var.vm_instances : vm.name => vm }
 
-#  source              = "./modules/instance"
-#  name                = each.value.name
-#  subnet_name         = each.value.subnet_name
-#  subnet_id           = module.network.subnet_ids[each.value.subnet_name]
-#  resource_group_name = var.resource_group_name
-#  location            = var.location
-#  ssh_public_key_path = var.ssh_public_key_path
-#}
 
 
 
