c1476146331568c0484354d658b55ccb9333e3ce
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-06-30 19:50:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,26 @@
 module "network" {
   source              = "./modules/network"
-  vnet_name           = "infra-development"
+  vnet_name           = var.vnet_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  address_space       = ["10.0.0.0/16"]
+  address_space       = var.address_space
 
   subnets = {
-    controlplane = "10.0.1.0/29"
-    worker       = "10.0.2.0/28"
-    lb           = "10.0.3.0/28"
+    aks-node-subnet = var.aks-node-subnet    
+    aks-lb-subnet          = var.aks-lb-subnet
   }
 }
-module "instances" {
-  for_each = { for vm in var.vm_instances : vm.name => vm }
+#module "instances" {
+#  for_each = { for vm in var.vm_instances : vm.name => vm }
 
-  source              = "./modules/instance"
-  name                = each.value.name
-  subnet_name         = each.value.subnet_name
-  subnet_id           = module.network.subnet_ids[each.value.subnet_name]
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  ssh_public_key_path = var.ssh_public_key_path
-}
+#  source              = "./modules/instance"
+#  name                = each.value.name
+#  subnet_name         = each.value.subnet_name
+#  subnet_id           = module.network.subnet_ids[each.value.subnet_name]
+#  resource_group_name = var.resource_group_name
+#  location            = var.location
+#  ssh_public_key_path = var.ssh_public_key_path
+#}
 
 
 
