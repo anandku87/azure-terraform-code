856ad9badb24eaf75260cc1b21a3ce3f7344501a
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-01 19:46:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,8 @@
 module "network" {
   source              = "./modules/network"
-  vnet_name           = var.vnet_name
   resource_group_name = var.resource_group_name
+  vnet_name           = var.vnet_name
   location            = var.location
   address_space       = var.address_space
-
-  subnets = {
-    aks-node-subnet = var.subnets["aks-node-subnet"]
-    aks-lb-subnet   = var.subnets["aks-lb-subnet"]
-  }
+  wn_subnet_prefix    = var.wn_subnet_prefix
 }
-
-
-
-
