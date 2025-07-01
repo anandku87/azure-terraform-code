fbfc72268081ebf4c36d2552ad7c216d8c625098
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-01 19:47:26 +0530

    Update variables.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,15 +1,19 @@
-variable "vnet_name" {
-  type = string
-}
-variable "address_space" {
-  type = list(string)
-}
-variable "location" {
-  type = string
-}
 variable "resource_group_name" {
   type = string
 }
-variable "subnets" {
-  type = map(string)
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "wn_subnet_prefix" {
+  type = string
 }
