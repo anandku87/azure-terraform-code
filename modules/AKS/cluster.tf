4dd46efc15d8a2f5e58a7c8d1950a4aa005d4194
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-06 12:35:56 +0530

    Update cluster.tf

--- modules/AKS/cluster.tf
+++ modules/AKS/cluster.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     load_balancer_sku  = var.load_balancer_sku
     service_cidr       = "10.2.0.0/16"
     dns_service_ip     = "10.2.0.10"
-    docker_bridge_cidr = "172.17.0.1/16"
+    #docker_bridge_cidr = "172.17.0.1/16"
   }
 
   api_server_access_profile {
