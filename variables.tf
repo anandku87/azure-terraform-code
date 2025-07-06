8b2090ec3409168705c9b074754098e120e904b3
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-06 11:35:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
+# Network Variables
 variable "subscription_id" {
   type = string
 }
+
 variable "resource_group_name" {
   type = string
 }
@@ -20,3 +22,66 @@ variable "address_space" {
 variable "wn_subnet_prefix" {
   type = string
 }
+
+# AKS Cluster Variables
+variable "aks_name" {
+  type = string
+}
+
+variable "dns_prefix" {
+  type = string
+}
+
+variable "enable_private_cluster" {
+  type    = bool
+  default = true
+}
+
+variable "node_vm_size" {
+  type    = string
+  default = "Standard_D2alds_v6"
+}
+
+variable "os_sku" {
+  type    = string
+  default = "Ubuntu"
+}
+
+variable "agent_pool_name" {
+  type    = string
+  default = "agentpool"
+}
+
+variable "agent_node_count" {
+  type    = number
+  default = 1
+}
+
+variable "user_node_pool_name" {
+  type    = string
+  default = "userpool"
+}
+
+variable "user_node_count" {
+  type    = number
+  default = 1
+}
+
+variable "min_node_count" {
+  type    = number
+  default = 1
+}
+
+variable "max_node_count" {
+  type    = number
+  default = 2
+}
+
+
+# Load Balancer Configuration
+variable "load_balancer_sku" {
+  description = "SKU for the AKS load balancer"
+  type        = string
+  default     = "standard"
+}
+
