60ac1b84c5ae95a69605166431f0ad4609cfb01f
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-18 16:43:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ module "network" {
   wn_subnet_prefix    = var.wn_subnet_prefix
 }
 
-module "AKS" {
-  source = "./modules/AKS"
-  aks_name = var.aks_name
-  dns_prefix = var.dns_prefix
-  user_node_pool_name = var.user_node_pool_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  wn_subnet_id    = module.network.wn_subnet_id
-}
+#module "AKS" {
+#  source = "./modules/AKS"
+#  aks_name = var.aks_name
+#  dns_prefix = var.dns_prefix
+#  user_node_pool_name = var.user_node_pool_name
+#  resource_group_name = var.resource_group_name
+#  location            = var.location
+#  wn_subnet_id    = module.network.wn_subnet_id
+#}
