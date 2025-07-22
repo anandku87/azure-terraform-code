9cef3798c081f50b4c338c83a393883e3e24e107
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-22 20:43:07 +0530

    Create output.tf

---
+++ modules/aks/output.tf
@@ -0,0 +1,23 @@
+output "aks_cluster_id" {
+  value       = azurerm_kubernetes_cluster.aks.id
+  description = "The ID of the AKS cluster"
+}
+
+output "aks_cluster_name" {
+  value       = azurerm_kubernetes_cluster.aks.name
+  description = "The name of the AKS cluster"
+}
+
+output "kube_config" {
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  description = "Raw Kubernetes configuration"
+  sensitive   = true
+}
+
+output "default_node_pool_name" {
+  value       = azurerm_kubernetes_cluster.aks.default_node_pool[0].name
+  description = "Name of the default agent node pool"
+}
+
+output "user_node_pool_name" {
+  value      
