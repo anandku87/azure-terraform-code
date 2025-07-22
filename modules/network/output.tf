09ed6e29777995fefd6114ed5f531eb63ab99434
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-22 20:22:49 +0530

    Delete modules directory

--- modules/network/output.tf
+++
@@ -1,8 +0,0 @@
-output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
-}
-
-output "wn_subnet_id" {
-  value       = azurerm_subnet.wn_subnet.id
-  description = "The ID of the worker node subnet"
-}
