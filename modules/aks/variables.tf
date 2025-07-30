63f0b363a716db7e58bfcf1ef44e5163395897b4
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-30 21:05:47 +0530

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -1,76 +1,84 @@
-# AKS cluster base variables
-variable "resource_group_name" {
-  type = string
+variable "cluster_preset" {
+  description = "Cluster preset configuration"
+  type        = string
+  default     = "production-economy"
 }
 
-variable "location" {
-  type = string
+variable "cluster_name" {
+  description = "AKS cluster name in format aks-region-appname-env"
+  type        = string
 }
 
-variable "aks_cluster_name" {
-  type = string
-}
-
-variable "dns_prefix" {
-  type = string
+variable "aks_pricing_tier" {
+  description = "AKS pricing tier"
+  type        = string
+  default     = "Free"
 }
 
 variable "kubernetes_version" {
-  type    = string
-  default = "1.29.0"
+  description = "Version of Kubernetes to use"
+  type        = string
+  default     = "latest"
 }
 
-variable "node_count" {
-  type    = number
-  default = 2
+variable "automatic_upgrade_enabled" {
+  description = "Enable automatic upgrades for cluster"
+  type        = bool
+  default     = false
 }
 
-variable "vm_size" {
-  type    = string
-  default = "Standard_DS2_v2"
+variable "node_security_channel" {
+  description = "Enable node OS security channel"
+  type        = bool
+  default     = false
 }
 
-variable "enable_private_network" {
-  type    = bool
-  default = true
+variable "user_node_pool_name" {
+  description = "Name of the default user node pool"
+  type        = string
+}
+
+variable "node_vm_size" {
+  description = "VM size for node pool"
+  type        = string
+  default     = "Standard_D2als_v6"
+}
+
+variable "node_count_min" {
+  description = "Minimum number of nodes in node pool"
+  type        = number
+  default     = 1
+}
+
+variable "node_count_max" {
+  description = "Maximum number of nodes in node pool"
+  type        = number
+  default     = 2
+}
+
+variable "enable_private_cluster" {
+  description = "Enable private AKS cluster"
+  type        = bool
+  default     = true
+}
+
+variable "use_custom_vnet" {
+  description = "Use custom Azure virtual network"
+  type        = bool
+  default     = true
+}
+
+variable "location" {
+  description = "Azure region where AKS is deployed"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
 }
 
 variable "subnet_id" {
-  type = string
-}
-
-# User-defined node pool variables
-variable "user_node_pool_name" {
-  type    = string
-  default = "userpool"
-}
-
-variable "user_node_pool_vm_size" {
-  type    = string
-  default = "Standard_DS3_v2"
-}
-
-variable "user_node_pool_node_count" {
-  type    = number
-  default = 2
-}
-
-variable "user_node_pool_os_type" {
-  type    = string
-  default = "Linux"
-}
-
-variable "enable_auto_scaling" {
-  type    = bool
-  default = true
-}
-
-variable "min_count" {
-  type    = number
-  default = 1
-}
-
-variable "max_count" {
-  type    = number
-  default = 5
+  description = "Subnet ID to attach AKS cluster to"
+  type        = string
 }
