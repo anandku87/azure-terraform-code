db797476c43e50b2ff8c880ee5564b24c259a22a
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-30 21:21:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,21 +8,22 @@ module "network" {
 }
 
 module "aks" {
-  source                = "./modules/aks"
-  resource_group_name   = var.resource_group_name
-  location              = var.location
-  cluster_name          = var.cluster_name
-  kubernetes_version    = var.kubernetes_version
-  aks_pricing_tier      = var.aks_pricing_tier
-  enable_private_cluster = var.enable_private_cluster
-  automatic_upgrade_enabled = var.automatic_upgrade_enabled
-  node_security_channel = var.node_security_channel
-  subnet_id             = module.network.wn_subnet_id
-  use_custom_vnet       = true
+  source                      = "./modules/aks"
+  resource_group_name         = var.resource_group_name
+  location                    = var.location
+  cluster_name                = var.cluster_name
+  kubernetes_version          = var.kubernetes_version
+  aks_pricing_tier            = var.aks_pricing_tier
+  enable_private_cluster      = var.enable_private_cluster
+  automatic_upgrade_enabled   = var.automatic_upgrade_enabled
+  node_security_channel       = var.node_security_channel
+  subnet_id                   = module.network.wn_subnet_id
+  use_custom_vnet             = var.use_custom_vnet
 
-  # 👇 Node pool settings
-  user_node_pool_name   = var.user_node_pool_name
-  node_vm_size          = var.user_node_pool_vm_size
-  node_count_min        = var.min_count
-  node_count_max        = var.max_count
+  user_node_pool_name         = var.user_node_pool_name
+  node_vm_size                = var.node_vm_size
+  node_count_min              = var.node_count_min
+  node_count_max              = var.node_count_max
+
+  cluster_preset              = var.cluster_preset
 }
