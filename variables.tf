187eb6e86eb2cd7b8d0bfc168b41e69dcd520699
anandku87 <89131735+anandku87@users.noreply.github.com>
2025-07-30 20:53:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,57 +25,73 @@ variable "wn_subnet_prefix" {
 
 ## AKS Cluster Details ##
 
-variable "aks_cluster_name" {
-  type = string
+variable "cluster_preset" {
+  description = "Cluster preset configuration"
+  type        = string
+  default     = "production-economy"
 }
-variable "dns_prefix" {
-  type = optional(string)
+
+variable "cluster_name" {
+  description = "AKS cluster name in format aks-region-appname-env"
+  type        = string
 }
+
+variable "aks_pricing_tier" {
+  description = "AKS pricing tier"
+  type        = string
+  default     = "Free"
+}
+
 variable "kubernetes_version" {
-  type = string
-}
-variable "node_count" {
-  type = number
-}
-variable "vm_size" {
-  type = string
-}
-variable "enable_private_network" {
-  type = bool
+  description = "Version of Kubernetes to use"
+  type        = string
+  default     = "latest"
 }
 
-# User-defined node pool variables
+variable "automatic_upgrade_enabled" {
+  description = "Enable automatic upgrades"
+  type        = bool
+  default     = false
+}
+
+variable "node_security_channel" {
+  description = "Enable Node security channel updates"
+  type        = bool
+  default     = false
+}
+
 variable "user_node_pool_name" {
-  type    = string
-  default = "userpool"
+  description = "Name of the user node pool"
+  type        = string
 }
 
-variable "user_node_pool_vm_size" {
-  type    = string
-  default = "Standard_DS3_v2"
+variable "node_vm_size" {
+  description = "VM size for node pool"
+  type        = string
+  default     = "Standard_D2als_v6"
 }
 
-variable "user_node_pool_node_count" {
-  type    = number
-  default = 2
+variable "node_count_min" {
+  description = "Minimum number of nodes"
+  type        = number
+  default     = 1
 }
 
-variable "user_node_pool_os_type" {
-  type    = string
-  default = "Linux"
+variable "node_count_max" {
+  description = "Maximum number of nodes"
+  type        = number
+  default     = 2
 }
 
-variable "enable_auto_scaling" {
-  type    = bool
-  default = true
+variable "enable_private_cluster" {
+  description = "Enable private AKS cluster"
+  type        = bool
+  default     = true
 }
 
-variable "min_count" {
-  type    = number
-  default = 1
+variable "use_custom_vnet" {
+  description = "Use custom Azure virtual network"
+  type        = bool
+  default     = true
 }
 
-variable "max_count" {
-  type    = number
-  default = 5
-}
